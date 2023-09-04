067b833c7f6b1d34571c312e31eb66e0bbdcf57f
Riel Journo <rielj@wix.com>
2023-09-04 10:24:15 +0300

    add basic functions

---
+++ server/dummyData.js
@@ -0,0 +1,38 @@
+export const menuData = [
+  {
+    id: 1,
+    data: "Home",
+    children: [],
+  },
+  {
+    id: 2,
+    data: "Products",
+    children: [3, 6],
+  },
+  {
+    id: 6,
+    data: "Smartphones",
+    children: [],
+  },
+
+  {
+    id: 7,
+    data: "Contact Us",
+    children: [],
+  },
+  {
+    id: 3,
+    data: "Laptops",
+    children: [4, 5],
+  },
+  {
+    id: 4,
+    data: "Gaming Laptops",
+    children: [],
+  },
+  {
+    id: 5,
+    data: "Business Laptops",
+    children: [],
+  },
+];
--- server/server.js
+++ server/server.js
@@ -1,9 +1,103 @@
-const express = require("express");
+import { menuData } from "./dummyData.js";
+import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3500;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// const menuData = [
+//   {
+//     id: 1,
+//     data: "Home",
+//     children: [],
+//   },
+//   {
+//     id: 2,
+//     data: "Products",
+//     children: [
+//       {
+//         id: 3,
+//         data: "Laptops",
+//         children: [
+//           {
+//             id: 4,
+//             data: "Gaming Laptops",
+//           },
+//           {
+//             id: 5,
+//             data: "Business Laptops",
+//           },
+//         ],
+//       },
+//       {
+//         id: 6,
+//         data: "Smartphones",
+//       },
+//     ],
+//   },
+//   {
+//     id: 7,
+//     data: "Contact Us",
+//   },
+// ];
+
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+const items = menuData;
+
+// Create (POST) a new user
+app.post("/items", (req, res) => {
+  const newUser = req.body;
+  items.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// Read (GET) all users
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+// Read (GET) a specific user by ID
+app.get("/items/:id", (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const item = items.find((i) => i.id === itemId);
+
+  if (!item) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    res.json(item);
+  }
+});
+
+// Update (PUT) a user by ID
+app.put("/items/:id", (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const updatedItem = req.body;
+  const index = items.findIndex((i) => i.id === itemId);
+
+  if (index === -1) {
+    res.status(404).json({ message: "Item not found" });
+  } else {
+    items[index] = { ...items[index], ...updatedItem };
+    res.json(items[index]);
+  }
+});
+
+// Delete (DELETE) a user by ID
+app.delete("/items/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const index = items.findIndex((i) => i.id === userId);
+
+  if (index === -1) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    items.splice(index, 1);
+    res.status(204).send();
+  }
 });
 
 app.listen(port, () => {
